04159dae7bf44d3d1ae2ed8375d4825088c97ea5
ana.medvedeva <anamedvedeva1@gmail.com>
2025-09-05 23:22:17 -0400

    Initial commit - Light/dark mode toggle functionality included

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground ">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      { /* Footer */}
+    </div>
+  )
+}
